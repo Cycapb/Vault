26c4cfffa5af0cf22b22cc18e1d32c55653c2eb9
Cycapb <aka.apostol@gmail.com>
2016-05-28 18:02:52 +0500

    Added controller TestController and view Index just for test how connection to database works.

---
+++ Vault/Vault/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Vault.Models;
+
+namespace Vault.Controllers
+{
+    public class TestController : Controller
+    {
+        private AppUserManager UserManager => HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+
+        public async Task<ActionResult> Index()
+        {
+            var user = await UserManager.FindByIdAsync("5749938fb3b6d23d540be565");
+            var list = new List<AppUserModel>();
+            if (user != null)
+            {
+                list.Add(user);
+            }
+            return View(list);
+        }
+    }
+}
