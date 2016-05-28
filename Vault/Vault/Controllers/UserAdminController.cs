c22458b3701030c34e18862d02484e0ed5eeb2cb
Cycapb <aka.apostol@gmail.com>
2016-05-28 19:19:07 +0500

    Created view for UserAdminController.Create. Wrote logic to create new users in method UserAdmin.Create()

---
+++ Vault/Vault/Controllers/UserAdminController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using MongoDB.Driver;
+using Vault.Infrastructure;
+using Vault.Models;
+
+namespace Vault.Controllers
+{
+    public class UserAdminController : Controller
+    {
+        private AppUserManager UserManager => HttpContext.GetOwinContext().GetUserManager<AppUserManager>();
+
+        private AppIdentityDbContext IdentityContext
+            => HttpContext.GetOwinContext().GetUserManager<AppIdentityDbContext>();
+
+        public async Task<ActionResult> Index()
+        {
+            var list = await IdentityContext.Users.Find(x=>true).ToListAsync();
+            return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(CreateModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var newUser = new AppUserModel() {UserName = userModel.Name, Email = userModel.Email};
+                var result = await UserManager.CreateAsync(newUser, userModel.Password);
+                if (result.Succeeded)
+                {
+                    RedirectToAction("Index");
+                }
+                else
+                {
+                    AddModelErrors(result);    
+                }
+            }
+            return View(userModel);
+        }
+
+        private void AddModelErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("",error);
+            }
+        }
+    }
+}
---
+++ Vault/Vault/Models/UserViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vault.Models
+{
+    public class CreateModel
+    {
+        [Required(ErrorMessage = "Field Name is empty")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Field Email is empty")]
+        public string Email { get; set; }   
+
+        [Required(ErrorMessage = "Field Password is empty")]
+        public string Password { get; set; }
+
+    }
+}
