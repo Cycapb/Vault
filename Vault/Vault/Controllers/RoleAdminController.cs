aaa49896355332cd8fcca10c8f347294f0264f3f
Cycapb <aka.apostol@gmail.com>
2016-05-29 13:04:29 +0500

    Created RoleAdminController and wrote logic in Index method. It recieves collection of roles.

---
+++ Vault/Vault/Controllers/RoleAdminController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using MongoDB.Driver;
+using Vault.Infrastructure;
+using Vault.Models;
+
+namespace Vault.Controllers
+{
+    public class RoleAdminController : Controller
+    {
+        private AppIdentityDbContext IdentityContext => HttpContext.GetOwinContext().GetUserManager<AppIdentityDbContext>();
+
+        private AppRoleManager RoleManager => HttpContext.GetOwinContext().GetUserManager<AppRoleManager>();
+
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                var list = await IdentityContext.Roles.Find(x => true).ToListAsync();
+                return View(list);
+            }
+            catch (Exception)
+            {
+                return View("Error", new string[] {"No connection to database"});
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
