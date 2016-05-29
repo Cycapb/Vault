c7de5bb7a79f6ab5e577d3f0157903f9aaef4a17
Cycapb <aka.apostol@gmail.com>
2016-05-29 13:45:23 +0500

    Added class .container to Site.css to use my own container for user and role representation. Created Index view for RoleAdmin.Index method.

--- Vault/Vault/Controllers/RoleAdminController.cs
+++ Vault/Vault/Controllers/RoleAdminController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using MongoDB.Driver;
 using Vault.Infrastructure;
