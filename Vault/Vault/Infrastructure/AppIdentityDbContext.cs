5797f29ce80482571bb53b96aabbacf1a9fcdff4
Cycapb <aka.apostol@gmail.com>
2016-05-29 12:49:59 +0500

    Created type AppRoleModel:IdentityRole

--- Vault/Vault/Infrastructure/AppIdentityDbContext.cs
+++ Vault/Vault/Infrastructure/AppIdentityDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using AspNet.Identity.MongoDB;
 using MongoDB.Driver;
 using Vault.Models;
 
@@ -8,18 +7,18 @@ namespace Vault.Infrastructure
     public class AppIdentityDbContext:IDisposable
     {
         public IMongoCollection<AppUserModel> Users { get; set; } 
-        public IMongoCollection<IdentityRole> Roles { get; set; } 
+        public IMongoCollection<AppRoleModel> Roles { get; set; } 
 
         public static AppIdentityDbContext Create()
         {
             var client = new MongoClient("mongodb://192.168.1.144:27017");
             var database = client.GetDatabase("IdentityDb");
             var users = database.GetCollection<AppUserModel>("users");
-            var roles = database.GetCollection<IdentityRole>("roles");
+            var roles = database.GetCollection<AppRoleModel>("roles");
             return new AppIdentityDbContext(users, roles);
         }
 
-        private AppIdentityDbContext(IMongoCollection<AppUserModel> users, IMongoCollection<IdentityRole> roles)
+        private AppIdentityDbContext(IMongoCollection<AppUserModel> users, IMongoCollection<AppRoleModel> roles)
         {
             Users = users;
             Roles = roles;
---
+++ Vault/Vault/Models/AppRoleModel.cs
@@ -0,0 +1,11 @@
+﻿using AspNet.Identity.MongoDB;
+
+namespace Vault.Models
+{
+    public class AppRoleModel:IdentityRole
+    {
+        public AppRoleModel() : base(){ }
+
+        public AppRoleModel(string name) : base(name) { }
+    }
+}
