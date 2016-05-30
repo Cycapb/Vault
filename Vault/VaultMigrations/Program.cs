f4b95574a5df9f7f0878870036bc535e648b536a
Cycapb <aka.apostol@gmail.com>
2016-05-30 15:05:36 +0500

    Wrote logic in migration tool which creates database if it doesn't exist.

--- Vault/VaultMigrations/Program.cs
+++ Vault/VaultMigrations/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace VaultMigrations
+﻿using System;
+using VaultMigrations.Abstract;
+using VaultMigrations.Concrete;
+using VaultMigrations.Models;
+
+namespace VaultMigrations
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Database initialization...");
+            IConnectionProvider provider = new MongoConnectionProvider();
+            AppIdentityDbContext context = AppIdentityDbContext.Create();
+            InitDb(provider,context);
+            Console.WriteLine("Migration finished");
+            Console.ReadLine();
         }
+
+        private static void InitDb(IConnectionProvider provider, AppIdentityDbContext context)
+        {
+            IExistingChecker checker = new MongoDbExistingChecker(provider);
+            if (!checker.Exist(provider.GetDatabase()).Result)
+            {
+                IDatabaseSeeder seeder = new MongoDbSeeder(provider, context);
+                seeder.Seed();
+            }
+        }
+
     }
 }
