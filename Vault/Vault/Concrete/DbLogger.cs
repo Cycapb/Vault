a6030f089920637f9a49632534bf1dc111cadb4a
Cycapb <aka.apostol@gmail.com>
2016-06-05 22:46:13 +0500

    Created class DbLogger:ILogger and implemented method Log. Created AccessLogmodel.

---
+++ Vault/Vault/Concrete/DbLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using Vault.Abstract;
+using Vault.Models;
+using VaultDAL.Abstract;
+using VaultDAL.Models;
+
+namespace Vault.Concrete
+{
+    public class DbLogger:ILogger
+    {
+        private readonly IRepository<VaultAccessLog> _repository;
+        public CreateLogModel LogModel { get; set; }
+
+        public DbLogger(IRepository<VaultAccessLog> repository)
+        {
+            _repository = repository;
+        }
+        public async Task Log(string message)
+        {
+            await SaveToDb(message);
+        }
+
+        private async Task SaveToDb(string message)
+        {
+            var logItem = new VaultAccessLog()
+            {
+                DateTime = DateTime.Now,
+                Event = message,
+                EventType = this.LogModel.EventType,
+                VaultId = this.LogModel.VaultId
+            };
+            await _repository.CreateAsync(logItem);
+        }
+    }
+}
---
+++ Vault/Vault/Models/AccessLogModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Vault.Models
+{
+    public class CreateLogModel
+    {
+        public string EventType { get; set; }
+        public string VaultId { get; set; }
+    }
+}
