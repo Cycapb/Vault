8d9298132ab0646f0cac83e506399691313559b9
Cycapb <aka.apostol@gmail.com>
2016-06-05 22:19:27 +0500

    Created model VaultAccessLog

---
+++ Vault/VaultDAL/Models/VaultAccessLog.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using VaultDAL.Concrete;
+
+namespace VaultDAL.Models
+{
+    public class VaultAccessLog:Entity
+    {
+        public string Event { get; set; }
+        public string EventType { get; set; }
+        public DateTime DateTime { get; set; }
+        public string VaultId { get; set; }
+    }
+}
