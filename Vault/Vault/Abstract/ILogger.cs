81201c84ffcfa34c2fc0ebe21602dacfeedeb97a
Cycapb <aka.apostol@gmail.com>
2016-06-05 17:16:17 +0500

    Added interface ILogger. Created class FileLogger:ILogger and implemented method Log.

---
+++ Vault/Vault/Abstract/ILogger.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Vault.Abstract
+{
+    public interface ILogger
+    {
+        Task Log(string message);
+    }
+}
---
+++ Vault/Vault/Concrete/FileLogger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ILogger = Vault.Abstract.ILogger;
+
+namespace Vault.Concrete
+{
+    public class FileLogger:ILogger
+    {
+        private readonly ExceptionContext _exceptionContext;
+
+        public FileLogger(ExceptionContext exceptionContext)
+        {
+            _exceptionContext = exceptionContext;
+        }
+
+        public async Task Log(string message)
+        {
+            await SaveToLogFile(message).ConfigureAwait(false);
+        }
+
+        private async Task SaveToLogFile(string error)
+        {
+            StringBuilder errorMessage = new StringBuilder();
+            errorMessage.AppendLine("");
+            errorMessage.AppendLine($"Date and time: {DateTime.Now}");
+            errorMessage.AppendLine($"User: {_exceptionContext.HttpContext.User.Identity.Name}");
+            errorMessage.AppendLine($"Controller: {_exceptionContext.RouteData.Values["controller"]}; Action: {_exceptionContext.RouteData.Values["action"]}");
+            errorMessage.AppendLine($"Error: {error}");
+            errorMessage.AppendLine($"Stack trace: {_exceptionContext.Exception.StackTrace}");
+
+            await CreateLogDir().ConfigureAwait(false);
+
+            using (Stream stream = new FileStream(@"C:\Programs\Logs\VaultLog.txt", FileMode.Append))
+            {
+                using (TextWriter writer = new StreamWriter(stream))
+                {
+                    await writer.WriteAsync(errorMessage.ToString()).ConfigureAwait(false);
+                }
+            }
+        }
+
+        private Task CreateLogDir()
+        {
+            return Task.Run(() =>
+            {
+                if (!Directory.Exists(@"C:\Programs\Logs"))
+                {
+                    Directory.CreateDirectory(@"C:\Programs\Logs");
+                }
+            });
+        }
+    }
+}
