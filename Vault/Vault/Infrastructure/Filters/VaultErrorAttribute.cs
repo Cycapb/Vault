8dbcc9092eac1ce7c5d69cb293f11460e92ef1a0
Cycapb <aka.apostol@gmail.com>
2016-06-05 17:37:18 +0500

    Created VaultErrorAttribute and ErrorPage view to show information to user.

---
+++ Vault/Vault/Infrastructure/Filters/VaultErrorAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Vault.Concrete;
+
+namespace Vault.Infrastructure.Filters
+{
+    public class VaultErrorAttribute:FilterAttribute,IExceptionFilter
+    {
+        public async void OnException(ExceptionContext filterContext)
+        {
+            var logger = new FileLogger(filterContext);
+            await logger.Log(filterContext.Exception.Message);
+            filterContext.Result = new ViewResult() {ViewName = "ErrorPage"};
+            filterContext.ExceptionHandled = true;
+        }
+    }
+}
