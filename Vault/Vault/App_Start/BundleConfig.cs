c2d7db22bac171c49f5aedd6d2bdf9cf2311a5b9
Cycapb <aka.apostol@gmail.com>
2016-06-08 11:37:42 +0500

    Updated jquery and bootstrap packages

--- Vault/Vault/App_Start/BundleConfig.cs
+++ Vault/Vault/App_Start/BundleConfig.cs
@@ -16,7 +16,9 @@ namespace Vault
                 "~/Scripts/jquery-{version}.js",
                 "~/Scripts/jquery.validate.js",
                 "~/Scripts/jquery.validate.unobtrusive.js"));
-
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
                 "~/Content/bootstrap.css",
